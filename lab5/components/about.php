c460c03d1801e881d3ceabc0f7d8c1035a66cdb6
NiNaz <98.nazola@mail.ru>
2021-06-24 12:44:57 +0300

    final_commit

---
+++ lab5/components/about.php
@@ -0,0 +1,8 @@
+<section class="about container">
+  <h2 class="section-title">Привет, я <?= $name ?></h2>
+  <img class="about-image" src="<?= $image ?>" width="147" height="147" alt="<?= $name ?>">
+  <div class="about-content">
+    <p>Меня зовут Никита. Работаю над 5 лабораторной. Учусь в магистратуре</p>
+    <p>Люблю гулять и играть на гитаре</p>
+  </div>
+</section>
---
+++ lab5/index.php
@@ -0,0 +1,16 @@
+<?php
+$title = 'Лаба 5';
+$name = 'Никита Назола';
+$image = 'img/redcandy.svg';
+$email = '98.nazola@mail.ru';
+
+
+require('components/header.php');
+require('components/status.php');
+require('components/about.php');
+
+require('components/photos.php');
+require('components/news.php');
+require('components/quotes.php');
+require('components/contacts.php');
+require('components/footer.php');
