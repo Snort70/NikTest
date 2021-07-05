bd1d0086b2cc67e7d0d92f4824ae323500a33276
NiNaz <98.nazola@mail.ru>
2021-07-05 18:06:51 +0300

    lab_10

--- skills.js
+++ skills.js
@@ -1,32 +1,25 @@
-  
-const skills = [
-    { css: "skill-html", skillName: "HTML", skillLevel: 60 },
-    { css: "skill-css", skillName: "CSS", skillLevel: 40 },
-    { css: "skill-php", skillName: "PHP", skillLevel: 30 },
+fetch('db/skills.json')
+    .then(data => data.json())
+    .then(skills => {
+        const dl = document.createElement('dl');
+        dl.classList.add('skills-list');
+        skills.forEach(skill => {
+            const dt = document.createElement('dt');
+            dt.textContent = skill.skillName;
+            dt.classList.add(skill.css);
 
-];
+            const dd = document.createElement('dd');
+            dd.classList.add('level');
 
+            const div = document.createElement('div');
+            div.style.width = skill.skillLevel + '%';
+            div.textContent = skill.skillLevel;
 
-function addSkills() {
-    const dl = document.createElement('dl');
-    dl.classList.add('skills-list');
-    skills.forEach(skill => {
-        const dt = document.createElement('dt');
-        dt.textContent = skill.skillName;
-        dt.classList.add(skill.css);
-
-        const dd = document.createElement('dd');
-        dd.classList.add('level');
-
-        const div = document.createElement('div');
-        div.style.width = skill.skillLevel + '%';
-        div.textContent = skill.skillLevel;
-        
-        dd.append(div);
-        dl.append(dt);
-        dl.append(dd);
-    });
-    document.querySelector('.skills').append(dl);
-}
-
-addSkills();
+            dd.appendChild(div);
+            dl.append(dt);
+            dl.append(dd);
+        });
+        document.querySelector('.skills').append(dl);
+      
+    }) 
+     .catch(() => console.error("упс, что-то пошло не так"));
